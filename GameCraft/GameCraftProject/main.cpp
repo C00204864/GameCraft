ddbf2d7acebdeaa085080b466155cb5d261bf48e
c00204864 <c00204864@itcarlow.ie>
2019-03-22 13:02:04 +0000

    Game Loop

--- GameCraft/GameCraftProject/main.cpp
+++ GameCraft/GameCraftProject/main.cpp
@@ -1,7 +1,25 @@
+#ifdef _DEBUG
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-audio-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "sfml-network-d.lib")
+#else
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-audio.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "sfml-network.lib")
+#endif
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+
 #include <iostream>
+#include "Game.h"
 
 int main()
 {
-	std::cout << "Start" << std::endl;
+	Game game;
+	game.run();
 	return 0;
 }
